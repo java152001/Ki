a9a6128a33572aff1d975fff080bb70716f5a246
irvingrivas <irving0594@gmail.com>
2018-12-22 13:43:15 -0700

    Created db Controllers

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -28,7 +28,7 @@ module.exports = {
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-      db.Comments
+      db.Comment
         .findById({ _id: req.params.id })
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
