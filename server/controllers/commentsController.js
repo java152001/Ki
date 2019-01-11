b772c4f4c287a52e1c964610cef3ca369cd056ac
irvingrivas <irving0594@gmail.com>
2019-01-10 20:59:57 -0700

    Finished db

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -3,42 +3,43 @@ const Article = require("mongoose").model('Article');
 
 // Defining methods for the CommentsController
 module.exports = {
-    findAll: function(req, res) {
-      Comment
-        .find(req.query)
-        .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    findById: function(req, res) {
-      Comment
-        .findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    create: function(req, res) {
-      console.log(req.body)
-      Comment
-        .create(req.body)
-        .then(function(dbModel) {
-          Article.findOneAndUpdate({_id: req.body.article}, { $push: { comments: dbModel._id } }, {new: true}).then(function(finArticle)
-          {
+  findAll: function (req, res) {
+    Comment
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    Comment
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    console.log(req.body)
+    Comment
+      .create(req.body)
+      .then(function (dbModel) {
+        Article.findOneAndUpdate({ _id: req.body.article },
+          { $push: { comments: dbModel._id } },
+          { new: true }).then(function (finArticle) {
             res.json(finArticle)
           })
-        })
-        .catch(err => res.status(422).json(err));
-    },
-    update: function(req, res) {
-      Comment
-        .findOneAndUpdate({ _id: req.params.id }, req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    },
-    remove: function(req, res) {
-      Comment
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    }
-  };
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    Comment
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    Comment
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
