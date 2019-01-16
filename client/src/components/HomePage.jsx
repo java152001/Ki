b09ad5c9fecf62860d6b79109707ba74ce9cc763
Benjamin Weinberger <benjaminweinberger@gmail.com>
2019-01-15 20:27:51 -0700

    superhero styling with bootswatch

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 import Auth from '../modules/Auth';
+import Jumbotron from '../components/Jumbotron'
 
 class HomePage extends React.Component {
 
@@ -11,14 +12,14 @@ class HomePage extends React.Component {
 
   render() {
     return (
-      <Card className="container">
+      <Jumbotron >
         <CardTitle title="React Application" subtitle="This is the home page." />
           {Auth.isUserAuthenticated() ? (
             <CardText style={{ fontSize: '16px', color: 'green' }}>Welcome! You are logged in.</CardText>
           ) : (
             <CardText style={{ fontSize: '16px', color: 'green' }}>You are not logged in.</CardText>
           )}
-      </Card>
+      </Jumbotron>
     )
   }
 };
